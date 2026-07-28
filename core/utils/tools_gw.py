7e19056d2506d462b205a902c932e8e7bc185581
Arnau Torret <arnautorret7@gmail.com>
2026-07-28 15:42:24 +0200

    fix(locale): update language retrieval to use QSettings for user locale

--- core/utils/tools_gw.py
+++ core/utils/tools_gw.py
@@ -27,7 +27,7 @@ from functools import partial
 from datetime import datetime
 
 from qgis.PyQt.QtCore import Qt, QStringListModel, QVariant, QDate, QRegularExpression, \
-    QItemSelectionModel, QTimer
+    QItemSelectionModel, QTimer, QSettings
 from qgis.PyQt.QtGui import QCursor, QPixmap, QColor, QStandardItemModel, QIcon, QStandardItem, \
     QIntValidator, QDoubleValidator, QRegularExpressionValidator, QPalette
 from qgis.PyQt.QtSql import QSqlTableModel
@@ -679,7 +679,7 @@ def create_body(form='', feature='', filter_fields='', extras=None, list_feature
     info_types = {'full': 1}
     plugin_version, message = tools_qgis.get_plugin_version()
     info_type = info_types.get(lib_vars.project_vars['info_type'])
-    lang = tools_qgis.get_ui_language_locale()
+    lang = QSettings().value("locale/userLocale")
 
     if body:
         body.setdefault('client', {"device": 4, "lang": lang, "version": f'"{plugin_version}"'})
