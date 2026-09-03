468532ed2054eb3f4922c3d64ce516894e19451a
Daniel Marín <dmarin@bgeo.es>
2026-09-03 12:43:54 +0200

    fix(gw_fct_linktonetwork): update `expl_visibility` to inherit correct values during link creation, ensuring consistency in test validations.

--- dbmodel/test/ws/function/test_gw_fct_linktonetwork.sql
+++ dbmodel/test/ws/function/test_gw_fct_linktonetwork.sql
@@ -26,7 +26,7 @@ VALUES(-903, '-903', NULL, 40.9000, NULL, 'CONNEC', 'DRINKING-FOUNTAIN', '-903',
 INSERT INTO connec (connec_id, code, sys_code, top_elev, "depth", feature_type, conneccat_id, customer_code, connec_length, epa_type, state, state_type, arc_id, expl_id, muni_id, sector_id, supplyzone_id, presszone_id, dma_id, dqa_id, omzone_id, crmzone_id, minsector_id, soilcat_id, function_type, category_type, location_type, fluid_type, n_hydrometer, n_inhabitants, staticpressure, descript, annotation, observ, "comment", link, num_value, district_id, postcode, streetaxis_id, postnumber, postcomplement, streetaxis2_id, postnumber2, postcomplement2, block_code, plot_id, workcat_id, workcat_id_end, workcat_id_plan, builtdate, enddate, ownercat_id, pjoint_id, pjoint_type, om_state, conserv_state, accessibility, access_type, placement_type, priority, brand_id, model_id, serial_number, asset_id, adate, adescript, verified, datasource, label_x, label_y, label_rotation, rotation, label_quadrant, inventory, publish, lock_level, expl_visibility, created_at, created_by, updated_at, updated_by, the_geom)
 VALUES(-904, '-904', NULL, 39.9200, NULL, 'CONNEC', 'ORNAMENTAL-FOUNTAIN', '-904', NULL, 'JUNCTION', 1, 2, NULL, 1, 1, 3, 0, 1, 1, NULL, 0, NULL, NULL, 'soil1', NULL, NULL, NULL, NULL, NULL, NULL, 63.930, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'work1', NULL, NULL, '2025-07-02', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, true, NULL, NULL, '2025-07-02 09:38:09.483', 'postgres', NULL, NULL, 'SRID=25831;POINT (419267.53202626767 4576826.267641516)'::public.geometry);
 
-UPDATE connec SET expl_visibility = ARRAY[-901]::smallint[] WHERE connec_id = -901;
+UPDATE connec SET expl_visibility = ARRAY[2]::smallint[] WHERE connec_id = -901;
 
 SELECT is(
     (gw_fct_setlinktonetwork($${"client":{"device":4, "lang":"es_ES", "version":"4.0.002", "infoType":1, "epsg":25831},
@@ -38,7 +38,7 @@ SELECT is(
 
 SELECT is(
     (SELECT expl_visibility FROM link WHERE feature_id = -901 AND state > 0 ORDER BY link_id DESC LIMIT 1),
-    ARRAY[-901]::smallint[],
+    ARRAY[2]::smallint[],
     'New/reconnected link inherits expl_visibility from connec'
 );
 
