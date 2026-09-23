38ceb2f5da554b009b54beab753c3f536afced88
Daniel Marín <dmarin@bgeo.es>
2026-09-23 13:25:26 +0200

    fix(dbjson): update SQL insert statements for node parameters to ensure correct ordering and structure

---
+++ dbmodel/schemas/main/common/updates/4/17/1/patch.sql
@@ -0,0 +1,20 @@
+/*
+This file is part of Giswater
+The program is free software: you can redistribute it and/or modify it under the terms of the GNU
+General Public License as published by the Free Software Foundation, either version 3 of the License,
+or (at your option) any later version.
+*/
+
+
+SET search_path = SCHEMA_NAME, public, pg_catalog;
+
+-- Toolbox 2118 (build nodes from arc vertices): node type combo, alphabetical
+UPDATE config_toolbox
+SET inputparams = replace(
+    inputparams::text,
+    'where cfn.id is not null',
+    'where cfn.id is not null order by cfn.id'
+)::json
+WHERE id = 2118
+  AND inputparams::text ILIKE '%where cfn.id is not null%'
+  AND inputparams::text NOT ILIKE '%order by cfn.id%';
